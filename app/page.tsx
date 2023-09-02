4e8d3ba4f640b5a4e9d2550d10b7528f82e1bea5
mayura-andrew <mayuraalahakoon@gmail.com>
2023-09-03 00:07:06 +0530

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
 "use client"
-
 import React from 'react';
 import NextHead from 'next/head';
-import Link from 'next/link';
 import Image from 'next/image';
-import MyComponent from '@/components/ImageBuilding';
 
 export default function Home() {
   return (
@@ -12,23 +9,23 @@ export default function Home() {
       <NextHead>
         <title>Your ultimate destination for free education</title>
       </NextHead>
-      <header className="bg-gradient-to-r to-blue-700 p-4 text-center flex items-center justify-between">
-        <div className="flex items-center text-center">
+      <header className="bg-gradient-to-r to-blue-700 p-1 text-center flex items-center justify-between align-middle">
+        <div className="flex justify-center"> {/* Center the image horizontally */}
           <Image
             src="/logo2.png"
             alt="knowlihub"
-            width={80}
-            height={50}
+            width={100}
+            height={80}
           />
         </div>
       </header>
-      <div className="text-3xl text-white text-center mt-3">Your ultimate destination for free education</div>
-      <main className="max-w-4xl mx-auto p-4 text-center mt-20 custom-font"> {/* Added custom-font class */}
-        <h1 className="text-4xl">Coming Soon...🚀</h1>
-        <p className="my-4">
+      <div className="text-3xl text-white text-center mt-2 font-extralight">Your ultimate destination for free education🎓</div>
+      <div className="text-4xl text-center align-middle mt-2  font-bold">Coming Soon...🚀</div>
+      <main className="max-w-4xl mx-auto p-4 text-center mt-2 font-mono">
+        <p className="my-4 font-mono">
           Welcome to our community-driven learning platform, where you can access the best online resources for free and without difficulty.
         </p>
-        <p>
+        <p className="font-mono"> {/* Apply a different font family */}
           Our vision is to provide better online resources for learners that are easily accessible and free of charge. We believe that everyone should have access to quality education, regardless of their background or financial situation.
         </p>
       </main>
@@ -36,7 +33,12 @@ export default function Home() {
       <style jsx>{`
         /* Define your custom font-family here */
         .custom-font {
-          font-family: 'Roboto', sans-serif; /* Use your desired font name */
+          font-family: 'Roboto', sans-serif;
+        }
+
+        /* Apply different font to the specific element */
+        .font-serif {
+          font-family: 'Font1', serif; /* Replace 'Font1' with your chosen serif font */
         }
       `}</style>
     </>

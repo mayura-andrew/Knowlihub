e2381bec94fb8777d0887d1d5fe2a004320b7670
mayura-andrew <mayuraalahakoon@gmail.com>
2023-09-02 23:45:10 +0530

    INIT

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from 'react';
 import NextHead from 'next/head';
 import Link from 'next/link';
@@ -15,14 +17,13 @@ export default function Home() {
           <Image
             src="/logo2.png"
             alt="knowlihub"
-            width={50}
+            width={80}
             height={50}
           />
-          <h1 className="text-3xl text-black ml-2">knowlihub</h1>
         </div>
       </header>
-        <h1 className="text-3xl text-white text-center mt-3">Your ultimate destination for free education</h1>
-      <main className="max-w-4xl mx-auto p-4 text-center mt-20">
+      <div className="text-3xl text-white text-center mt-3">Your ultimate destination for free education</div>
+      <main className="max-w-4xl mx-auto p-4 text-center mt-20 custom-font"> {/* Added custom-font class */}
         <h1 className="text-4xl">Coming Soon...🚀</h1>
         <p className="my-4">
           Welcome to our community-driven learning platform, where you can access the best online resources for free and without difficulty.
@@ -31,6 +32,13 @@ export default function Home() {
           Our vision is to provide better online resources for learners that are easily accessible and free of charge. We believe that everyone should have access to quality education, regardless of their background or financial situation.
         </p>
       </main>
+
+      <style jsx>{`
+        /* Define your custom font-family here */
+        .custom-font {
+          font-family: 'Roboto', sans-serif; /* Use your desired font name */
+        }
+      `}</style>
     </>
   );
 }

75e6ca2ad06c7db3950eae6306a077706b2b0e71
mayura-andrew <mayuraalahakoon@gmail.com>
2023-11-17 17:47:11 +0530

    fixed next.config

--- next.config.js
+++ next.config.js
@@ -2,3 +2,13 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+
+// next.config.js
+module.exports = {
+    output: {
+      // This is the directory where 'next export' will write the static HTML files
+      // Make sure this directory doesn't conflict with your version control or other files
+      path: 'out',
+    },
+  }
+  
